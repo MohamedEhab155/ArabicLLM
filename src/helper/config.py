fc39bedb23e307348856f9cfa0c7723349678d5e
Mohamed Ehab <ma0587580@gmail.com>
2026-02-11 20:19:09 +0200

    fix: update output directory path and reset story ID for evaluation

--- src/helper/config.py
+++ src/helper/config.py
@@ -5,8 +5,8 @@ TORCH_DTYPE = None
 
 #FILE_PATH_SAMPLE_STORY = r"/kaggle/working/ArabicLLM---test_kaggle/data/row/stories_sample.jsonl" 
 FILE_PATH_SAMPLE_STORY = r"data/row/stories_sample.jsonl"
-OUTPUT_DIR = r"/kaggle/working/ArabicLLM---test_kaggle/outputs"
-STORY_ID_TO_EVALUATE:int=3
+OUTPUT_DIR = r"E:\NLP_Industry_Ready\ArabicLLM - test_kaggle\outputs"
+STORY_ID_TO_EVALUATE:int=1
 TARGET_LANGUAGE="ar"
 
 TASK_MAPPING = [
