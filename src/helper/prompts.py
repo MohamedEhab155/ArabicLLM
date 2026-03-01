ea4e6425e4316591310cfcb932b67a0ccd1a519d
Mohamed Ehab <ma0587580@gmail.com>
2026-03-01 20:13:34 +0200

    add : to save  all prompts

---
+++ src/helper/prompts.py
@@ -0,0 +1,6 @@
+NLP_PASER_FORMATER= "\n".join([
+    "You are a professional NLP data parser.",
+    "Follow the provided `Task` by the user and the `Output Scheme` to generate the `Output JSON`.",
+    "Do not generate any introduction or conclusion."
+]) 
+
