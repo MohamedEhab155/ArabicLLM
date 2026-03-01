2ba3912f9006d49ae7e7e8a32972ba60dacee538
Mohamed Ehab <ma0587580@gmail.com>
2026-03-01 20:13:51 +0200

    add datadir var

--- src/helper/config.py
+++ src/helper/config.py
@@ -1,8 +1,9 @@
 BASE_MODEL_ID = "Qwen/Qwen2.5-1.5B-Instruct"
-DATA_DIR = "/gdrive/MyDrive/youtube-resources/llm-finetuning"
+DATA_path = "/gdrive/MyDrive/youtube-resources/llm-finetuning"
 DEVICE = "cuda" 
 TORCH_DTYPE = None
 
+DATA_DIR=r"E:\NLP_Industry_Ready\ArabicLLM - test_kaggle\data"
 #FILE_PATH_SAMPLE_STORY = r"/kaggle/working/ArabicLLM---test_kaggle/data/row/stories_sample.jsonl" 
 FILE_PATH_SAMPLE_STORY = r"data/row/stories_sample.jsonl"
 OUTPUT_DIR = r"E:\NLP_Industry_Ready\ArabicLLM - test_kaggle\outputs"
